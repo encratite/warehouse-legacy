4d71c55a48fd3123f6806c5525dbc5675531413d
void <git.email.address@googlemail.com>
2010-05-01 04:16:41 +0200

    Forgot to add the password script

---
+++ scripts/password.rb
@@ -0,0 +1,35 @@
+def getByte
+	data = []
+	
+	ranges =
+	[
+		'A'..'Z',
+		'a'..'z',
+		'0'..'9'
+	]
+	
+	ranges.each do |range|
+		data += range.to_a
+	end
+	
+	return data[rand(data.size)]
+end
+
+def getPassword(length)
+	output = ''
+	length.times { output += getByte }
+	return output
+end
+
+def runProgram
+	if ARGV.empty?
+		puts 'Usage:'
+		puts "ruby #{File.basename(__FILE__)} <length>"
+		return
+	end
+
+	length = ARGV[0].to_i
+	puts getPassword(length)
+end
+
+runProgram

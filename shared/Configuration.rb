48449ccc8696b8699b11bdda1167f32947d5c869
void <git.email.address@googlemail.com>
2010-05-03 02:07:39 +0200

    Forgot to save the config

--- shared/Configuration.rb
+++ shared/Configuration.rb
@@ -1,16 +1,6 @@
 require 'nil/environment'
 
 module Configuration
-	module Cookie
-		UId = '953675'
-		Pass = 'ab31c2bdf48e5e9d60d19b7f40cf0de0'
-	end
-	
-	module IRC
-		Server = 'irc.sceneaccess.org'
-		Nick = 'malleruet'
-	end
-	
 	module Database
 		Adapter = 'postgres'
 		Host = '127.0.0.1'
@@ -19,14 +9,26 @@ module Configuration
 		Database = 'warehouse'
 	end
 	
-	module ReleaseChannel
-		Channel = '#scc-announce'
-		Nick = 'SCC'
-		Host = 'csops.sceneaccess.org'
+	module SceneAccess
+		module Cookie
+			UId = '953675'
+			Pass = 'ab31c2bdf48e5e9d60d19b7f40cf0de0'
+		end
 		
-		module Regexp
-			Release = /\-\> ([^ ]+) \(Uploaded/
-			URL = /(http\:\/\/[^\)]+)\)/
+		module IRC
+			Server = 'irc.sceneaccess.org'
+			Nick = 'malleruet'
+		end
+		
+		module ReleaseChannel
+			Channel = '#scc-announce'
+			Nick = 'SCC'
+			Host = 'csops.sceneaccess.org'
+			
+			module Regexp
+				Release = /\-\> ([^ ]+) \(Uploaded/
+				URL = /(http\:\/\/[^\)]+)\)/
+			end
 		end
 	end
 	

3731dfefc5acad5072ee57cb75524f45d3254ab0
void <git.email.address@googlemail.com>
2010-05-23 01:14:03 +0200

    Time to run the first tests on the JSON RPC server

--- rpc/JSONRequest.rb
+++ rpc/JSONRequest.rb
@@ -1,38 +1,18 @@
 require 'json'
 require 'cgi'
 
-class JSONRequest
-	attr_reader :address, :requests, :cookies
-	
-	attr_writer :user
+require 'www-library/HTTPRequest'
+
+class JSONRequest < HTTPRequest
+	attr_reader :jsonRequests
 	
 	def initialize(environment)
-		@address = environment['HTTP_X_REAL_IP']
-		
-		@cookies = {}
-		cookies = environment['HTTP_COOKIE']
-		if cookies != nil
-			cookieTokens = cookies.split(';').map { |token| token.strip }
-			cookieTokens.each do |token|
-				assignmentTokens = token.split '='
-				next if assignmentTokens.size != 2
-				variable, value = assignmentTokens
-				value = CGI::unescape value
-				@cookies[variable] = value
-			end
-		end
-		
-		@environment = environment
-		
-		@user = nil
+		super(environment)
+		processJSON
 	end
 	
 	def processJSON
 		lines = environment['rack.input'].read.split("\n")
-		@requests = lines.map{|x| JSON.parse(x)}
-	end
-	
-	def isLoggedIn
-		return @user != nil
+		@jsonRequests = lines.map{|x| JSON.parse(x)}
 	end
 end
--- rpc/JSONServer.rb
+++ rpc/JSONServer.rb
@@ -7,13 +7,28 @@ require 'shared/database'
 require 'shared/User'
 
 require 'www-library/RequestManager'
+require 'www-library/RequestHandler'
+require 'www-library/HTTPReply'
 
 class JSONServer
+	RequestHandlers =
+	{
+		'sum' => [:sumRPC, [Fixnum, Fixnum]],
+		'login' => [:loginRPC, [String, String]],
+	}
+	
 	def initialize(sessionCookie, log)
 		@sessionCookie = sessionCookie
 		@output = OutputHandler.new(log)
 		@database = getDatabase
-		@requestManager = RequestManager.new
+		initialiseRequestManager
+	end
+	
+	def initialiseRequestManager
+		@requestManager = RequestManager.new(JSONRequest)
+		mainHandler = RequestHandler.new(nil)
+		mainHandler.setHandler(method(:indexHandler))
+		@requestManager.addHandler(mainHandler)
 	end
 	
 	def output(request, line)
@@ -21,29 +36,93 @@ class JSONServer
 	end
 	
 	def processRequest(environment)
-		request = JSONRequest.new(environment)
-		begin
-			request.processJSON
-		rescue JSON::ParserError => exception
-			output(request, "JSON error: #{exception.message}")
-			return
-		end
-		
-		readSessionData(request)
+		@requestManager.handleRequest(environment)
 	end
 	
-	def readSessionData(request)
+	def getUser(request)
 		sessionString = request.cookies[@sessionCookie]
-		return false if sessionString == nil
+		return nil if sessionString == nil
 		sessions = @database[:user_session]
 		results = sessions.join(:user_data, id: :user_id).where(ip: request.address, session_string: sessionString).filter(:id, :name, :is_administrator).all
 		if results.empty?
 			output "Received an invalid session string from #{request.address}: #{sessionString}"
-			return false
+			return nil
 		end
 		result = results[0]
-		request.user = User.new(result)
-		output "Recognised the session string of user #{request.user.name}"
-		return true
+		user = User.new(result)
+		output "Recognised the session string of user #{user.name}"
+		return user
+	end
+	
+	def error(message, id)
+		output =
+		{
+			'error' => message,
+			'id' => id
+		}
+		
+		return output
+	end
+	
+	def processJSONRPCRequest(jsonRequest, user)
+		#{"id":1,"method":"system.about","params":[]}
+		id = jsonRequest['id']
+		method = jsonRequest['method']
+		arguments = jsonRequest['params']
+		
+		handlerData = RequestHandlers[method]
+		if handlerData == nil
+			return error("The method \"#{method}\" does not exist", id)
+		end
+		
+		handlerMethod = method(handlerData[0])
+		handlerArguments = handlerData[1]
+		if arguments.size != handlerArguments.size
+			return error("The argument counts for the method \"#{method}\" mismatch (expected: #{handlerArguments.size}, given: #{arguments.size})", id)
+		end
+		
+		offset = 0
+		while offset < arguments.size
+			argument = arguments[offset]
+			argumentType = handlerArguments[offset]
+			if !(argumentType === argument)
+				return error("The argument type of argument #{offset + 1} for method \"#{method}\" is invalid (expected: #{argumentType}, given: #{argument.class})", id)
+			end
+			offset += 1
+		end
+		
+		begin
+			result = handlerMethod(*arguments)
+			
+			output =
+			{
+				'result' => result,
+				'error' => nil,
+				'id' => id
+			}
+			
+			return output
+		rescue RuntimeError => exception
+			return error(exception.message, id)
+		end
+	end
+	
+	def indexHandler(request)
+		user = getUser(request)
+		output = ''
+		request.jsonRequests.each do |jsonRequest|
+			output.concat(JSON.unparse(processJSONRPCRequest(jsonRequest, user)) + "\n")
+		end
+		reply = HTTPReply.new(output)
+		reply.contentType = 'application/json-rpc'
+		return reply
+	end
+	
+	def sumRPC(a, b)
+		return a + b
+	end
+	
+	def loginRPC(username, password)
+		return nil
 	end
 end

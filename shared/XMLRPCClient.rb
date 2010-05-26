d6b79efb9ae2b363d561c7e49a266a4a37d5fd85
void <git.email.address@googlemail.com>
2010-05-27 01:16:21 +0200

    Trying to speed up the XML RPC

--- shared/XMLRPCClient.rb
+++ shared/XMLRPCClient.rb
@@ -5,11 +5,22 @@ class XMLRPCClient
 		@host = host
 		@port = port
 		@path = path
+		@client = nil
 	end
 	
 	def call(*arguments)
-		client = XMLRPC::Client.new(@host, @path, @port)
-		output = client.call(*arguments)
-		return output
+		tries = 2
+		tries.times do
+			if @client == nil
+				@client = XMLRPC::Client.new(@host, @path, @port)
+			end
+			begin
+				output = @client.call(*arguments)
+				return output
+			rescue Errno::EPIPE
+				@client = nil
+			end
+		end
+		raise XMLRPC::FaultException.new('Broken pipe')
 	end
 end

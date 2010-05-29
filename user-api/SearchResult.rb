8057a33ab5a1a55c4c698d306e8707af3eae7de0
void <git.email.address@googlemail.com>
2010-05-29 19:13:22 +0200

    Added seeder count to the search results

--- user-api/SearchResult.rb
+++ user-api/SearchResult.rb
@@ -15,6 +15,8 @@ class SearchResult < JSONObject
 		@section = data[:section_name]
 		@size = data[:release_size]
 		@date = data[:release_date].utc
+		#warning - this data is pretty useless and outdated - can help on old torrents though I suppose
+		@seederCount = data[:seeder_count]
 		@descriptions = []
 		@id = data[:site_id]
 		processData(site, @id, @date)

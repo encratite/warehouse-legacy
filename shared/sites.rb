38eab4354f799f41d6eb43836b5b38a7ab0f5172
void <git.email.address@googlemail.com>
2010-11-07 06:25:36 +0100

    Moved TL to the top for the shell stuff - more convenient

--- shared/sites.rb
+++ shared/sites.rb
@@ -11,8 +11,8 @@ require 'configuration/Configuration'
 
 def getReleaseSites(connections)
 	return [
+		TorrentLeechSite.new(TorrentLeechConfiguration, Configuration::Torrent, connections, Configuration),
 		SceneAccessSite.new(SceneAccessConfiguration, Configuration::Torrent, connections, Configuration),
 		TorrentVaultSite.new(TorrentVaultConfiguration, Configuration::Torrent, connections, Configuration),
-		TorrentLeechSite.new(TorrentLeechConfiguration, Configuration::Torrent, connections, Configuration),
 	]
 end

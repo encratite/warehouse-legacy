368d878306782cbac2f984c45a185bf39b85427d
void <git.email.address@googlemail.com>
2013-12-02 00:52:31 +0100

    Implemented TL login

--- site/torrentleech/TorrentLeechSite.rb
+++ site/torrentleech/TorrentLeechSite.rb
@@ -3,6 +3,45 @@ require 'shared/http/HTTPReleaseSite'
 class TorrentLeechSite < HTTPReleaseSite
   def initialize(siteData, torrentData, connections, configuration)
     super(siteData, torrentData, connections, configuration)
+    @httpData = siteData::HTTP
+  end
+
+  def login
+    postData = {
+      'login' => 'submit',
+      'password' => @httpData::Password,
+      'remember_me' => 'on',
+      'username' => @httpData::Username,
+    }
+    @httpHandler.post('/user/account/login/', postData)
+  end
+
+  def browse(retrying = false)
+    data = @httpHandler.get(@browsePath)
+    if data == nil
+      output "Error: Failed to retrieve #{@browsePath}"
+      return
+    end
+
+    releases = @htmlParser.processData(data)
+    if releases.empty?
+      output "Error: Failed to retrieve any releases from #{@browsePath}"
+      if retrying
+        output 'Aborted'
+      else
+        output 'Logging in and trying again'
+        login
+        browse(true)
+      end
+      return
+    end
+    output "Retrieved #{releases.size} releases from #{@browsePath}"
+    releases.each do |release|
+      result = @dataset.where(site_id: release.siteId)
+      #check if this release is already in the database
+      next if !result.empty?
+      processNewRelease release
+    end
   end
 
   def processNewRelease(release)
